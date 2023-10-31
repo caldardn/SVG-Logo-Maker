2b5d2d5983e0aede375631e6549f00c62ca7921f
caldardn <caldardn@gmail.com>
2023-10-31 10:47:35 -0400

    adding corrected createdsvg shapes and index .js code

--- index.js
+++ index.js
@@ -1,72 +1,66 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
-const {createSVG} = require('./lib/createSVG');
-
+const fs = require("fs");
+const { createSVG } = require("./lib/createSVG");
+const validColor = ["red", "green", "blue", "yellow", "orange", "purple"]
 const questions = [
-    {
-      type: "input",
-      message: "Please enter up to three characters to be displayed for you logo.",
-      name: "userText",
-      default: "DNC",
-      // validate: (input) => {
-      //   if(input.length > 3 ){
-      //       return true
-      //   }else{
-      //       return "Please enter up to three characters."
-      //   }
-      // }
-    },
-    {
-        type: "input",
-        message: "Please enter what text color you would like.",
-        name: "userTextColor",
-        default: "Blue",
-        // validate: (input) => {
-        //     if(colorChoice(input)){
-        //         return true
-        //     }else{
-        //         return "Please enter a valid color"
-        //     }
-        //   }
-    },
-    {
-        type: "list",
-        message: "Please select the shape you would like to have.",
-        name: "userShape",
-        default: "Circle",
-        choice:[
-            "Circle",
-            "Triangle",
-            "Square"
-        ]
-     },
-     {
-        type: "input",
-        message: "Please enter the color you would like for your shape.",
-        name: "userShapeColor",
-        default: "Green",
-        // validate: (input) => {
-        //     if(colorChoice(input)){
-        //         return true
-        //     }else{
-        //         return "Please enter a valid color"
-        //     }
-        //   }
-      },
-]
+  {
+    type: "input",
+    message:
+      "Please enter up to three characters to be displayed for you logo.",
+    name: "userText",
+    default: "DNC",
+    validate: (input) => {
+      if(input.length <= 3 ){
+          return true
+      }else{
+          return "Please enter up to three characters."
+      }
+    }
+  },
+  {
+    type: "input",
+    message: "Please enter what text color you would like.",
+    name: "userTextColor",
+    default: "Blue",
+    validate: (input) => {
+        if(validColor.includes(input.toLowerCase())){
+            return true
+        }else{
+            return "Please enter a valid color"
+        }
+      }
+  },
+  {
+    type: "list",
+    message: "Please select the shape you would like to have.",
+    name: "userShape",
+    default: "Circle",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    message: "Please enter the color you would like for your shape.",
+    name: "userShapeColor",
+    default: "Green",
+    validate: (input) => {
+      if(validColor.includes(input.toLowerCase())){
+          return true
+      }else{
+          return "Please enter a valid color"
+      }
+    }
+  },
+];
 
 function init() {
-  inquirer.prompt(questions)
-  .then((data) =>
-  {
-    const svgContent = createSVG(data)
+  inquirer.prompt(questions).then((data) => {
+    const svgContent = createSVG(data);
 
-    fs.writeFile('logo.svg',svgContent,(err)=>
-    err ? console.error(err) :console.log ('SVG created'))
-  }
-  );
+    fs.writeFile("./dist/logo.svg", svgContent, (err) =>
+      err ? console.error(err) : console.log("SVG created")
+    );
+  });
 }
-
 
 init();
 
